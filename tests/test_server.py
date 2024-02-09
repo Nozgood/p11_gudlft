459969c8694319967419ce8824067b973047e8b2
nowfeel <nowfeel@bliink.io>
2024-02-09 11:15:00 +0100

    test: edit unit test to verify clubs are displayed on html pages

--- tests/test_server.py
+++ tests/test_server.py
@@ -4,18 +4,21 @@ from tests.conftest import client
 def test_should_return_index(client):
     response = client.get('/')
     assert response.status_code == 200
+    assert "test_one@test.com" in response.get_data(as_text=True)
 
 
 def test_show_summary_unknown_email(client):
     response = client.post('/showSummary', data={'email': 'unknown@gmail.com'}, follow_redirects=True)
     assert 'Unknown email address' in response.get_data(as_text=True)
     assert response.status_code == 200
+    assert "test_one@test.com" in response.get_data(as_text=True)
 
 
 def test_show_summary_valid_email(client):
     response = client.post('showSummary', data={"email": "test_one@test.com"})
     assert 'Unknown email address' not in response.get_data(as_text=True)
     assert 'Welcome, test_one@test.com' in response.get_data(as_text=True)
+    assert 'Clubs:' in response.get_data(as_text=True)
 
 
 def test_purchase_places_competition_not_enough_points(client):
