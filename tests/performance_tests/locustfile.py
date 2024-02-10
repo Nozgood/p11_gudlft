6f75fa5707561d59063849d38ffcc1a40c68e907
nowfeel <nowfeel@bliink.io>
2024-02-10 18:13:14 +0100

    test: add all methods to performance tests with locust

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -3,16 +3,33 @@ from locust import HttpUser, task
 
 class ProjectPerfTest(HttpUser):
     @task
-    def home(self):
+    def index(self):
         self.client.get('/')
 
     @task
-    def refresh_page(self):
+    def show_summary(self):
+        self.client.post(
+            '/showSummary',
+            {
+                'email': 'john@simplylift.co'
+            }
+        )
+
+    @task
+    def purchase_places(self):
         self.client.post(
             '/purchasePlaces',
             {
                 "club": "Simply Lift",
                 "competition": "Testok",
-                "places": "2"
+                "places": "1"
             }
         )
+
+    @task
+    def book(self):
+        self.client.get('/book/Testok/Simply%20Lift')
+
+    @task
+    def logout(self):
+        self.client.get('/logout')
