3a58dca0c8baad7f28af19c4705e5a57399d42e9
nowfeel <nowfeel@bliink.io>
2024-02-21 07:20:54 +0100

    fix: mock of competitions and clubs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,48 @@
 import pytest
-from server import app, load_competitions, load_clubs
-
-
-def load_mock_clubs():
-    # lire le contenu du fichier
-    return load_clubs()
-
-
-def load_mock_competitions():
-    return load_competitions()
+import server
 
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
-    app.clubs = load_mock_clubs()
-    app.competitions = load_mock_competitions()
-
-    with app.test_client() as client:
+    server.app.config['TESTING'] = True
+    with server.app.test_client() as client:
         yield client
+
+
+@pytest.fixture(autouse=True)
+def competitions_fixture(monkeypatch):
+    mock_competitions = [
+        {
+            "name": "past festival",
+            "date": "2024-01-20 10:00:00",
+            "numberOfPlaces": "100"
+        },
+        {
+            "name": "good behavior",
+            "date": "2028-01-20 10:00:00",
+            "numberOfPlaces": "100"
+        },
+        {
+            "name": "one place",
+            "date": "2028-01-20 10:00:00",
+            "numberOfPlaces": "1"
+        }
+    ]
+    monkeypatch.setattr(server, 'competitions', mock_competitions)
+
+
+@pytest.fixture(autouse=True)
+def clubs_fixture(monkeypatch):
+    mock_clubs = [
+        {
+            "name": "first test",
+            "email": "test_one@test.com",
+            "points": "1"
+        },
+        {
+            "name": "second test 12 points",
+            "email": "test_two@test.com",
+            "points": "30"
+        }
+    ]
+    monkeypatch.setattr(server, 'clubs', mock_clubs)
